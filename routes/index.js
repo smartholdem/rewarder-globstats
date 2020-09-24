5b5a7bcad686736278724de80072f90ce90defa3
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2020-09-24 18:56:50 +0300

    ipArr

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 /* post: new rewarder activation notification */
 router.post('/', async function(req, res, next) {
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  let ipArr = ip.split(':');
+  //console.log(req.connection)
+  console.log(ipArr[ipArr.length - 1])
   await res.json(req.body)
 });
 
